984cb8d99a9ada1aceea0a39ac9f1a9d66cfdfec
Andrew-Bogolubof <a.bogolubof@gmail.com>
2019-05-18 11:50:34 +0300

    Improve adding of moving pizzas to DOM

--- views/js/main.js
+++ views/js/main.js
@@ -526,6 +526,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var fragment = document.createDocumentFragment();
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
@@ -534,7 +535,8 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    fragment.appendChild(elem);
   }
+  document.querySelector("#movingPizzas1").appendChild(fragment);
   updatePositions();
 });
